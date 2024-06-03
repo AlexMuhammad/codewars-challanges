001d17ed7b52366fac50797d9dfb12d50ff6ec1f
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2024-06-03 18:22:56 +0700

    [KYU-8]: dna to rna solutions

---
+++ kyu-8/5-dna-to-rna-conversion.js
@@ -0,0 +1,8 @@
+function DNAtoRNA(dna) {
+  //    create a function which returns an RNA sequence from the given DNA sequence
+  //    return dna.replace(/T/g, "U");
+  //    return dna.toUpperCase().split("").map((item) => item === "T" ? "U" : item).join("");
+  //    return dna.replaceAll("T", "U");
+}
+
+console.log(DNAtoRNA("TTTTTTT"));
